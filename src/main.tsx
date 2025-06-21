1d96357865ff83f69f0b7f4cf35c1994a296ef2c
Alexey Ushakov <develengie@gmail.com>
2025-06-21 14:48:18 +0300

    chore: registered Service Worker

--- src/main.tsx
+++ src/main.tsx
@@ -11,3 +11,10 @@ createRoot(document.getElementById("root")!).render(
         </BrowserRouter>
     </StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("/sw.js")
+        .then((reg) => console.log("Service Worker registered.", reg))
+        .catch((error) => console.log("Service Worker not registered.", error));
+}
